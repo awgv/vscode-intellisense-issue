31fddeea3e76980be9d37228ef363e03f5b47e8c
Andrew Golubev <aagolubev@gmail.com>
2020-08-13 16:49:53 +0300

    Change comments

--- src/Components/Foo/Bar/Bar.d.ts
+++ src/Components/Foo/Bar/Bar.d.ts
@@ -1,9 +1,12 @@
 type Props = {
   /** You get a list of available props by pressing Ctrl+Space while inspecting
-   * `<Bar />` in Foo.js, but won’t be able to enjoy the same functionality in a
+   * `<Bar />` in Foo.js, but won’t be able to enjoy the same functionality in the
    * Bar.js itself.
    */
   readMe: boolean
 }
 
+/**
+ * The Bar component’s comment.
+ */
 export declare function Bar(props: Props): React.FunctionComponent
